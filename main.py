db8288441fe3e357a4e4d2284c42aa2d3e4b6055
Martin Binamé <marti>
2023-05-16 14:54:06 +0200

    Adding Tests and error handler

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ def list_patients():
         print(patient)
 
 def save_new_diagnosis(name, diagnosis):
+    if name == "" or diagnosis =="":
+        print("Could not save patient and diagnosis due to invalide input")
+        return
     final_diagnosis = name + ": " + diagnosis
     patients_and_diagnosis.append(final_diagnosis)
     print("Final Diagnosis:", final_diagnosis, "\n")
@@ -35,15 +38,19 @@ def save_new_diagnosis(name, diagnosis):
 # Try calling the 2 functions below according to the appearance_prompt input!
 def assess_skin(skin):
     if skin == "1":
-        return no_dehydratation
+        return some_dehydratation
     elif skin == "2":
         return severe_dehydratation
+    else:
+        return ""
 
 def assess_eyes(eyes):
     if eyes == "1":
         return no_dehydratation
     elif eyes == "2":
         return severe_dehydratation
+    else:
+        return ""
     
 def assess_apperance():
     appearance = input(appearance_prompt)
@@ -53,6 +60,8 @@ def assess_apperance():
     elif appearance == "2":
         skin = input(skin_prompt)
         return assess_skin(skin)
+    else:
+        return ""
 
 
 def start_new_diagnosis():
@@ -75,3 +84,51 @@ def main():
             return
 
 main()
+
+# Assessing all if-then-else cases for both skin and eyes
+def test_assess_skin():
+    print(assess_skin("1") == some_dehydration)
+    print(assess_skin("2") == severe_dehydration)
+    print(assess_skin("3") == "")
+
+def test_assess_eyes():
+    print(assess_eyes("1") == no_dehydration)
+    print(assess_eyes("2") == severe_dehydration)
+    print(assess_eyes("3") == "")
+
+def test_assess_appearance():
+    print(assess_appearance())
+
+# Assessing all possible cases of entries for name and diagnosis
+def test_save_new_diagnosis():
+    save_new_diagnosis("", "")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration"
+    ])
+    save_new_diagnosis("Nimish", "")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration"
+    ])
+    save_new_diagnosis("", "No dehydration")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration"
+    ])
+    save_new_diagnosis("Nimish", "Some dehydration")
+    print(patients_and_diagnoses == [
+        "Mike - Severe dehydration",
+        "Sally - No dehydration",
+        "Kate - Some dehydration",
+        "Nimish - Some dehydration"
+    ])
+
+
+#test_assess_skin()
+#test_assess_eyes()
+#test_assess_appearance()
+#test_save_new_diagnosis()
