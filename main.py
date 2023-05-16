45b6cc90d73ec70c393f61a6fad9838421b0e249
Martin Binamé <marti>
2023-05-16 14:58:49 +0200

    Handling syntax error

--- main.py
+++ main.py
@@ -87,40 +87,40 @@ main()
 
 # Assessing all if-then-else cases for both skin and eyes
 def test_assess_skin():
-    print(assess_skin("1") == some_dehydration)
-    print(assess_skin("2") == severe_dehydration)
+    print(assess_skin("1") == some_dehydratation)
+    print(assess_skin("2") == severe_dehydratation)
     print(assess_skin("3") == "")
 
 def test_assess_eyes():
-    print(assess_eyes("1") == no_dehydration)
-    print(assess_eyes("2") == severe_dehydration)
+    print(assess_eyes("1") == no_dehydratation)
+    print(assess_eyes("2") == severe_dehydratation)
     print(assess_eyes("3") == "")
 
 def test_assess_appearance():
-    print(assess_appearance())
+    print(assess_apperance())
 
 # Assessing all possible cases of entries for name and diagnosis
 def test_save_new_diagnosis():
     save_new_diagnosis("", "")
-    print(patients_and_diagnoses == [
+    print(patients_and_diagnosis == [
         "Mike - Severe dehydration",
         "Sally - No dehydration",
         "Kate - Some dehydration"
     ])
     save_new_diagnosis("Nimish", "")
-    print(patients_and_diagnoses == [
+    print(patients_and_diagnosis == [
         "Mike - Severe dehydration",
         "Sally - No dehydration",
         "Kate - Some dehydration"
     ])
     save_new_diagnosis("", "No dehydration")
-    print(patients_and_diagnoses == [
+    print(patients_and_diagnosis == [
         "Mike - Severe dehydration",
         "Sally - No dehydration",
         "Kate - Some dehydration"
     ])
     save_new_diagnosis("Nimish", "Some dehydration")
-    print(patients_and_diagnoses == [
+    print(patients_and_diagnosis == [
         "Mike - Severe dehydration",
         "Sally - No dehydration",
         "Kate - Some dehydration",
